c31d0e69ea047831ef92da1ad0a11f5e98177ff9
Stephen Darling <stephen.darling@dailypay.com>
2025-06-18 06:40:43 -0400

    fix: add keymap to center after scroll up/down half-page

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set({ "n", "x" }, "<C-u>", "<C-u>zz", { desc = "Scroll Half Page Up (Centered)" })
+vim.keymap.set({ "n", "x" }, "<C-d>", "<C-d>zz", { desc = "Scroll Half Page Down (Centered)" })
