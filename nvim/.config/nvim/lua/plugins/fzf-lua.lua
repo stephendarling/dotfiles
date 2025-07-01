4ba0d1d56380e910b7cd4a9dc1aebdd259ad2662
Stephen Darling <stephen.darling@outlook.com>
2025-06-30 21:46:51 -0400

    fix: ripgrep for fzf-lua

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -2,19 +2,14 @@ return {
   "ibhagwan/fzf-lua",
   config = function()
     local actions = require("fzf-lua").actions
-
-    -- Sessions/directories where hidden files should be shown
-    local show_hidden_names = { "dotfiles", "personal" }
-
-    local tmux_session = vim.fn.system("tmux display-message -p '#S' 2>/dev/null"):gsub("%s+", "")
-    local cwd_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-
-    local show_hidden = vim.tbl_contains(show_hidden_names, tmux_session)
-      or vim.tbl_contains(show_hidden_names, cwd_name)
-
     require("fzf-lua").setup({
       files = {
-        hidden = show_hidden,
+        cmd = "rg --files --hidden --glob '!.git/*'",
+      },
+      oldfiles = {
+        cwd_only = true, -- Only show files from current working directory
+        prompt = "Recent Files (cwd)> ",
+        stat_file = true, -- Verify files still exist
       },
       actions = {
         files = {
