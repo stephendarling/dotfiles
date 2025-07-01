74adf29d465c4d75d5134fc27e3227c548698c97
Stephen Darling <stephen.darling@outlook.com>
2025-06-30 20:47:45 -0400

    fix: cleanup tmux

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -3,9 +3,18 @@ return {
   config = function()
     local actions = require("fzf-lua").actions
 
+    -- Sessions/directories where hidden files should be shown
+    local show_hidden_names = { "dotfiles", "personal" }
+
+    local tmux_session = vim.fn.system("tmux display-message -p '#S' 2>/dev/null"):gsub("%s+", "")
+    local cwd_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+
+    local show_hidden = vim.tbl_contains(show_hidden_names, tmux_session)
+      or vim.tbl_contains(show_hidden_names, cwd_name)
+
     require("fzf-lua").setup({
       files = {
-        hidden = false,
+        hidden = show_hidden,
       },
       actions = {
         files = {
