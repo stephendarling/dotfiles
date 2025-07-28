46931f715ec8912cab61954c76e06ddcc43299ed
Stephen Darling <stephen.darling@dailypay.com>
2025-07-27 21:09:50 -0400

    fix: Add filetype extension block

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.filetype.add({
+  extension = {
+    tf = "terraform",
+  },
+})
+
 neoscroll = require("neoscroll")
 local keymap = {
   ["<C-u>"] = function()
